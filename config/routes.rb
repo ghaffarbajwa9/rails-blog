87ea10b1cb52405e6597e48cc1fe7e08e87d209d
ghaffarbajwa9 <ghaffarbajwa9@gmail.com>
2022-05-26 12:58:57 +0500

    First blog is created in articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  root "articles#index"
+
+  get "/articles", to: "articles#index"
+end
+
+Rails.application.routes.draw do
+  get "/articles", to: "artilces#index"
+
+end
+Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
